d90897bf0da99bfbc4290c2c16ea88d7d9be9226
Milena Maia <milenamaiaaraujo@gmail.com>
2023-06-12 10:39:46 -0300

    :tada: initial commit

---
+++ MiniME/MiniME/MiniMEApp.swift
@@ -0,0 +1,20 @@
+//
+//  MiniMEApp.swift
+//  MiniME
+//
+//  Created by Milena Maia Araújo on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MiniMEApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
