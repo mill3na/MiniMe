a825e9f8a6a68a3ee57746d5f362b00690c17c99
canogg <88619478+canogg@users.noreply.github.com>
2023-06-16 10:04:24 -0300

    primeiras telas

--- MiniME/MiniME/View/MinimeView.swift
+++ MiniME/MiniME/View/MinimeView.swift
@@ -7,37 +7,40 @@
 
 import Foundation
 import SwiftUI
+import CloudKit
 
 struct MinimeView: View {
 
+    let container = CKContainer(identifier: "iCloud.miniMe")
     var body: some View {
-        VStack {
-            Image("minime")
-                .scaledToFit()
-                .frame(width: 300, height: 300)
-                .padding(.bottom, 100)
-                .padding(.top, 150)
+        NavigationStack {
+            VStack {
+                Image("minime")
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .padding(.bottom, 100)
+                    .padding(.top, 150)
 
-            Button {
-                //                activity()
-            } label: {
-                Text("Nova atividade")
+                NavigationLink {
+                    NovAtividade(viewModel: TaskViewModel(container: self.container))
+                } label: {
+                    Text("Nova atividade")
 
-                    .foregroundColor(.black)
-                    .background {
-                        Rectangle()
-                            .frame(width: 180, height: 60)
-                            .foregroundColor(.white)
-                            .cornerRadius(12)
-                            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 3) // << shadow to all composition
-                    }
+                        .foregroundColor(.black)
+                        .background {
+                            Rectangle()
+                                .frame(width: 180, height: 60)
+                                .foregroundColor(.white)
+                                .cornerRadius(12)
+                                .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 3) // << shadow to all composition
+                        }
+
+                }
+                .padding(.top, 70)
 
             }
-            .padding(.top, 70)
-
         }
     }
-
 }
 
 struct MinimeView_Previews: PreviewProvider {
